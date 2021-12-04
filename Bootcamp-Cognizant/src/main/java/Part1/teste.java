87c57837cca351db88225c1496f2a0d7ba966b72
s4u10 <saulobfilho@gmail.com>
2021-12-04 07:58:21 -0300

    second
    second

---
+++ Bootcamp-Cognizant/src/main/java/Part1/teste.java
@@ -0,0 +1,14 @@
+package Part1;
+
+public class teste {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cognizant");
+
+        int i = 3;
+        int b =  10 - 5 * 2 + --i;
+
+        System.out.println(b);
+    }
+}
